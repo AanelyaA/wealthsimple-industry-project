cc7a9842d332f1040e490080660d0fc1fc7a8eff
Sharon Jacob <fab8925@gmail.com>
2024-05-31 11:14:23 -0400

    Updated Styles and added an Invest Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import InvestProfile from './components/InvestProfile/InvestProfile';
+import Quiz from './components/QuizList/QuizList';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>WealthSimple FLEX App</h1>
-    </>
-  )
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {/* <Route path="/" element={<HomePage />} /> */}
+                    {/* <Route path="/login" element={<Login />} /> */}
+                    <Route path="/quiz" element={<Quiz />} />
+                    <Route path="/" element={<InvestProfile />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
