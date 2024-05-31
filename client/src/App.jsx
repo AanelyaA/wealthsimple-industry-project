a89077f9b479d5a677128030357f58d1a2b2d816
stephdream <stephyaowei@gmail.com>
2024-05-31 10:53:04 -0400

    Finished the landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import IntroPage from './pages/IntroPage/IntroPage';
+import LandingPage from './pages/LandingPage/LandingPage';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<IntroPage/>}/>
+        <Route path="/" element={<LandingPage />} />
+        <Route path='/intro' element={<IntroPage/>}/>
         <Route path="/login" element={<LoginPage/>} />
       </Routes>
     </Router>
