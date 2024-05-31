22fbb5fcb66f01f524270d974c13eac73b6625fb
Sharon Jacob <fab8925@gmail.com>
2024-05-31 15:08:18 -0400

    adding investrofile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
-// import IntroPage from "./pages/IntroPage/IntroPage";
+import IntroPage from "./pages/IntroPage/IntroPage";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import InvestProfile from "./components/InvestProfile/InvestProfile";
 import PersonalisedQuestionnaire from "./components/PersonalisedQuestionnaire/PersonalisedQuestionnaire";
@@ -8,24 +8,22 @@ import QuizList from "./components/QuizList/QuizList";
 import ScaleQuestionnaire from "./components/ScaleQuestionnaire/ScaleQuestionnaire";
 import "./App.scss";
 
-
 function App() {
   return (
     <Router>
       <div className="App">
-
         <Routes>
-          {/* <Route path="/" element={<LandingPage />} /> */}
           <Route path="/intro" element={<IntroPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/welcome1" element={<ScaleQuestionnaire />} />
           <Route path="/welcome2" element={<PersonalisedQuestionnaire />} />
           <Route path="/quizzes" element={<QuizList />} />
+          <Route path="/landing" element={<LandingPage />} />
           <Route path="/" element={<InvestProfile />} />
         </Routes>
       </div>
-
     </Router>
   );
 }
+
 export default App;
---
+++ client/src/components/ReferralModal/ReferralModal.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './ReferralModal.scss';
+
+const ReferralModal = ({ isOpen, onClose }) => {
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal-content">
+                <button className="close-button" onClick={onClose}>&times;</button>
+                <div className="modal-header">
+                    <img src="/path/to/your/image.png" alt="Referral Image" />
+                    <h2>Refer Friends and Earn More</h2>
+                </div>
+                <div className="modal-body">
+                    <p>Refer your friends to FLEX and earn extra rewards. For each friend who signs up and makes a deposit of $500 or more, you’ll earn an additional 0.5% interest.</p>
+                    <p><strong>April earnings so far: $0.01</strong></p>
+                </div>
+                <div className="modal-footer">
+                    <button className="learn-more-button">Learn More &rarr;</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ReferralModal;
