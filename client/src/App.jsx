aa454d925ce98cde6b041cf6fc131e3b779f4806
stephdream <stephyaowei@gmail.com>
2024-05-31 14:25:00 -0400

    solved conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import IntroPage from "./pages/IntroPage/IntroPage";
+// import IntroPage from "./pages/IntroPage/IntroPage";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import InvestProfile from "./components/InvestProfile/InvestProfile";
 import PersonalisedQuestionnaire from "./components/PersonalisedQuestionnaire/PersonalisedQuestionnaire";
@@ -15,7 +15,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<LandingPage />} />
-//           <Route path="/intro" element={<IntroPage />} />
+          {/* <Route path="/intro" element={<IntroPage />} /> */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/welcome" element={<PersonalisedQuestionnaire />} />
           <Route path="/quizzes" element={<QuizList />} />
